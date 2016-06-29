a69b2f26828d2d3fb652013a4e14548d4c9b7375
joestephens <joe.stephens@live.co.uk>
2016-06-29 10:35:05 +0100

    Merged with approval changes and fixed tests

--- spec/features/request_confirmation_spec.rb
+++ spec/features/request_confirmation_spec.rb
@@ -1,8 +1,9 @@
 feature 'User can approve requests' do
 
-  scenario 'Can see pending requests' do
+  before(:each) do
     signup
     listSpace
+    click_button 'sign out'
     signup(email: 'ken@ken.com')
     visit '/spaces'
     click_link "view space"
@@ -10,19 +11,13 @@ feature 'User can approve requests' do
 		click_button "confirm request"
     signin
     visit '/requests'
+  end
+
+  scenario 'Can see pending requests' do
     expect(page).to have_content("Luke's place")
   end
 
   scenario 'Can approve request' do
-    signup(email: 'bob@bob.com')
-    listSpace
-    signup(email: 'ben@ben.com')
-    visit '/spaces'
-    click_link "view space"
-		click_link "2016-08-16"
-		click_button "confirm request"
-    signin(email: 'bob@bob.com')
-    visit '/requests'
     click_button "Approve"
     expect(Request.first.confirmed).to eq 2
   end
