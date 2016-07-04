0b2c495701c4e190034b847c976809a783a5339c
joestephens <joe.stephens@live.co.uk>
2016-07-04 10:14:44 +0100

    Refactored requests

--- spec/units/request_spec.rb
+++ spec/units/request_spec.rb
@@ -7,11 +7,11 @@ describe Request do
   let(:available_dates) {[available_date1,available_date2]}
 
   it 'returns true when Requested date is included in available_dates' do
-    expect(Request.date_valid(available_dates, Date.parse('2016-08-09'))).to be true
+    expect(Request.valid_date?(available_dates, Date.parse('2016-08-09'))).to be true
   end
 
   it 'returns true when Requested date is included in available_dates' do
-    expect(Request.date_valid(available_dates, Date.parse('2016-08-11'))).to be false
+    expect(Request.valid_date?(available_dates, Date.parse('2016-08-11'))).to be false
   end
 
 end
